f1c894f255290225a0ef4d878ab14307e44f72bc
mayunlong <865147643@qq.com>
2022-02-23 09:29:27 +0800

    feat：实现 effect 返回 runner 功能

--- src/reactivity/effect/effect.ts
+++ src/reactivity/effect/effect.ts
@@ -17,7 +17,7 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 
@@ -64,4 +64,5 @@ let activeEffect;
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn);
   _effect.run();
+  return _effect.run.bind(_effect);
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -14,4 +14,19 @@ describe("effect", () => {
     user.age++;
     expect(nextAge).toBe(12);
   });
+  it("should return runner when call effect", () => {
+    // effect(fn) -> function (runner) -> fn() -> return
+    // 调用 effect  返回一个 runner 函数 效用runner  函数 返回用户传入fn 的返回值
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+    // 测试 effect 执行
+    expect(foo).toBe(11);
+    // 执行 effect 返回的函数
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
 });
