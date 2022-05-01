be0b86b61aad90e110863a9e5f31b60bd2f2343c
一纸信笺落廖尘 <865147643@qq.com>
2022-05-01 18:23:36 +0800

    feat: transform 逻辑实现

--- README.md
+++ README.md
@@ -42,6 +42,9 @@
 > - 解析 element 逻辑
 > - 解析 text 逻辑
 > - 解析三种联合类型 逻辑
+> - 有限状态机 (parse 图解)
+> - transform 逻辑
+
 
 > 课程学习地址
 > ![dce7da28dbf31cf13f2e1d8a2585e29](https://user-images.githubusercontent.com/29727848/159952869-17f82eec-2a21-4dfd-95fa-bb589549c9a0.jpg)
---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,40 @@
+import { NodeTypes } from "./ast";
+
+// 1. 深度优先搜索
+// 2. 修改 text content
+export function transform(root, options) {
+  const context = createTransformsContext(root, options);
+  // 1. 深度优先搜索
+  traverseNode(root, context);
+}
+// 处理传入的 ast 树 深度遍历寻找需要修改的 值
+function traverseNode(node: any, context) {
+  //  console.log(node);
+  // 为了达到测试效果 判断 node.type 是不是 text 如果是 就进行修改
+  // 2. 修改 text content
+  //   if (node.type === NodeTypes.TEXT) {
+  //     node.content = node.content + "my-mini-vue";
+  //   }
+  const nodeTransforms = context.transforms;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    nodeTransforms[i] && nodeTransforms[i](node);
+  }
+  traverseChildren(node, context);
+}
+// 处理 node children
+function traverseChildren(node, context) {
+  const children = node.children;
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i];
+      traverseNode(node, context);
+    }
+  }
+}
+
+function createTransformsContext(root, options) {
+  return {
+    root,
+    transforms: options.nodeTransforms || [],
+  };
+}
